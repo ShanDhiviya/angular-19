fa338576bc8f9ad1c509e135442f05a99a106c60
ShanDhiviya <132169598+ShanDhiviya@users.noreply.github.com>
2025-04-21 17:09:58 +0530

    Add homepage and profile components, update configuration files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:'', component: HomepageComponent},
+  { path: 'profile', component: ProfileComponent }
+];
